0b14e4556cf75ab92b8ab078f471d33e47ca56f4
bacha <bachaeshetu@gmail.com>
2024-12-11 22:36:40 +0300

    these are some string methodes

---
+++ python1/string.py
@@ -0,0 +1,9 @@
+name = input(" what is your name ")
+#len() method : which is tell us the length of the string
+#esult1 = len(name)
+# find() method: return the position of the staring (the occurence)
+#upper() and lower()
+#result2 = name.find("e")
+#result3 = name.lower()
+result4 = name.isdigit()
+print(result4)
